45c9e7a8c1e22ba54ee4f3fb8a6975d5de325489
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2024-03-07 19:54:11 +0530

    methods

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -11,21 +11,21 @@
 // int maxVal = Math.Max(x1,x2);
 // Console.WriteLine(maxVal);
 
-string[] arr = new string[3];
-arr[0] = "helllo";
-arr[1] = "eeded";
+// string[] arr = new string[3];
+// arr[0] = "helllo";
+// arr[1] = "eeded";
 // arr[5] = "ded";
 
 // System.Console.WriteLine(arr[0]);
 // System.Console.WriteLine(arr[1]);
 
-string[] arr1 = ["helo", "raw", "tent"];
+// string[] arr1 = ["helo", "raw", "tent"];
 // System.Console.WriteLine(arr1[0]);
 // System.Console.WriteLine(arr1.Length);
 
-foreach(string i in arr1){
+// foreach(string i in arr1){
     // System.Console.WriteLine(i);
-}
+// }
 
 // int [] arrInt = [1,3,4,2,4,5,3,2,3,4,3,1];
 // int sum = 0;
@@ -65,20 +65,52 @@ foreach(string i in arr1){
 // The term widening conversion means that you're attempting to convert a value from a data 
 // type that could hold less information to a data type that can hold more information
 
-int value1 = (int)1.5m; // upcast
-int value2 = Convert.ToInt32(1.5m); // downcast
+// int value1 = (int)1.5m; // upcast
+// int value2 = Convert.ToInt32(1.5m); // downcast
 // System.Console.WriteLine($" {value1} {value2}");
 
-string[] values = ["12.3", "45", "ABC", "11", "DEF"];
-string message = "";
-decimal total = 0;
-foreach(string i in values){
+// string[] values = ["12.3", "45", "ABC", "11", "DEF"];
+// string message = "";
+// decimal total = 0;
+// foreach(string i in values){
 
-    decimal num = 0;
-    if(decimal.TryParse(i, out num)){
-        total += num;
-    }else{
-        message += i; 
-    }
-}
-System.Console.WriteLine($"{total} {message}");
+//     decimal num = 0;
+//     if(decimal.TryParse(i, out num)){
+//         total += num;
+//     }else{
+//         message += i; 
+//     }
+// }
+// System.Console.WriteLine($"{total} {message}");
+
+// methods
+
+// System.Console.WriteLine(sum(1,2));
+
+// int sum(int a, int b){
+//     return a+b;
+// }
+
+// int sumOfArr(int[] arr){
+//     int ans = 0;
+//     foreach(int i in arr) ans += i;
+//     return ans;
+// }
+
+// int[] arr = [1,2,3,4,3,2,1];
+// System.Console.WriteLine(sumOfArr(arr));
+
+// string str = "hello";
+// int[] arr = [1,2,3,5];
+// strChange(str);
+// arrChange(arr);
+// System.Console.WriteLine(arr[3]);
+// System.Console.WriteLine(str);
+
+// void arrChange(int[] arr){
+//     arr[3] = 4;
+// }
+// void strChange(string str){
+//     str = "bye";
+//     return;
+// }
